806b33a23dc01fe814a67a5f656c49a1167ec3ef
Ihtisham Ullah <ahsaanullah174@gmail.com>
2023-02-22 05:20:34 +0500

    changes

--- hardhat-tutorial/contracts/Exchange.sol
+++ hardhat-tutorial/contracts/Exchange.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Exchange is ERC20 
 {
     address public cryptoDevTokenAddress;
-    constructor (address _CryptoDevToken) ERC20 ("CryptoDev LP Token", "CDLP")
+    constructor (address _CryptoDevToken) ERC20("CryptoDev LP Token", "CDLP")
     {
         require (_CryptoDevToken != address(0), "Token address passed is a null address");
         cryptoDevTokenAddress = _CryptoDevToken;
